07a44bd9214ba656f093a50523326be7448030db
Alan Killian <akillian@outlook.com>
2023-02-19 18:34:30 -0500

    Commit. Register working ok.

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import SteinStore from 'stein-js-client';
+import accessAttendeesTables from './modules/accessAttendeesTables';
 
 // Create Stein Store instance
 const VITE_STEIN_URL = import.meta.env.VITE_STEIN_URL;
@@ -26,12 +27,9 @@ button.addEventListener('click', function () {
     if (isValid) {
         // Form passed validity check, lets grab data from form
         let formData = new FormData(form);
-        let arrayFormData = [];
-        // form.classList.add('form--no');
 
-        // Need to convert FormData object to array object for stein to accept.
+        // Need to convert FormData object to array object for Stein to accept.
         formData = Object.fromEntries(formData.entries());
-        arrayFormData.push(formData);
 
         // Build an array of objects to send to stein as group
         let teamMember = {};
@@ -39,11 +37,12 @@ button.addEventListener('click', function () {
 
         // Iterate thru the array and check if any group member
         // names are null so we dont send them to Google sheets
-        for (let i = 1; i < 5; i++) {
-            if (formData['student_name' + i]) {
+        // Note- the magic number 6 is the maximum number of players(5) in form we can submit
+        for (let i = 1; i < 6; i++) {
+            if (formData['player_name' + i]) {
                 teamMember['school'] = formData['school'];
                 teamMember['teacher_advisor'] = formData['teacher_advisor'];
-                teamMember['student_name'] = formData['student_name' + i];
+                teamMember['player_name'] = formData['player_name' + i];
                 teamMember['ranking'] = formData['ranking' + i];
 
                 // Push students in group onto an array
@@ -56,7 +55,7 @@ button.addEventListener('click', function () {
 
         // Let's hit the stein API with the form data in array form
         try {
-            store.append('signup', teamMembers).then((res) => {
+            store.append('registration', teamMembers).then((res) => {
                 console.log(res);
                 form.reset();
                 button.innerHTML = `Sign-up succesful. ${res.updatedRange}`;
@@ -78,3 +77,11 @@ button.addEventListener('click', function () {
 });
 
 // ===== End Event listener to submit form data =================== //
+
+// ===== Event listener for accessing attendees table ==================== //
+
+// Sign-up button listener
+signUp.addEventListener('click', () => {
+    console.log("I've been clicked!.");
+    accessAttendeesTables();
+});
---
+++ modules/accessAttendeesTables.js
@@ -0,0 +1,64 @@
+import SteinStore from 'stein-js-client';
+import sortTable from './sortTable';
+
+const tableTemplate = document.getElementById('target');
+
+// Create Stein Store instance
+const VITE_STEIN_URL = import.meta.env.VITE_STEIN_URL;
+const store = new SteinStore(VITE_STEIN_URL);
+
+// Event listener for close button on table
+tableTemplate.addEventListener('click', (e) => {
+    const el = e.target;
+    if (el.classList.contains('close')) {
+        tableTemplate.classList.toggle('visible');
+    }
+});
+
+/**
+ * Description - Retrieves google sheets via the Stein JS client
+ * imports - sortTable
+ * Called from - /main.js
+ * Returns - Google sheets signup table and then uses Handlebars.js
+ *           to inject data into HTML template
+ */
+
+export default function accessAttendeesTables() {
+    let attendeesArray = [];
+    if (tableTemplate.classList.contains('visible')) {
+        tableTemplate.classList.remove('visible');
+    }
+
+    try {
+        store
+            .read('signup')
+            .then((data) => {
+                attendeesArray = data;
+
+                // ===== Handlebars template code ============ //
+                let template = document.getElementById('template').innerHTML;
+
+                //Compile the template
+                let compiled_template = Handlebars.compile(template);
+
+                //Render the data into the template
+                let rendered = compiled_template({ attendeesArray });
+
+                //Overwrite the contents of #target with the renderer HTML
+                document.getElementById('target').innerHTML = rendered;
+
+                // toggle the form block so table is able to take its place in
+                // document body.
+                user.classList.toggle('visible');
+            })
+            .then(() => {
+                sortTable();
+                crowdImg.classList.toggle('visible');
+                tableTemplate.scrollIntoView({ behavior: 'smooth' });
+
+                //  ============= End Template code =============== //
+            });
+    } catch (error) {
+        console.error(error);
+    }
+}
